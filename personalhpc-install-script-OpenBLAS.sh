c69f7aa2102da7b4cfb5a403ec7ef81a7181166d
Maximilien Levesque <maximilien.levesque@gmail.com>
2015-03-21 15:01:53 +0100

    Update personalhpc-install-script-OpenBLAS.sh

--- personalhpc-install-script-OpenBLAS.sh
+++ personalhpc-install-script-OpenBLAS.sh
@@ -3,5 +3,5 @@ cd /tmp
 git clone https://github.com/xianyi/OpenBLAS.git OpenBLAS   # clone last version of OpenBLAS
 cd /tmp/OpenBLAS
 make # DYNAMIC_ARCH=1 USE_THREAD=1
-sudo make PREFIX=/opt/OpenBLAS    # make the dir one wants 
+sudo make PREFIX=/opt/OpenBLAS install    # make the dir one wants 
 rm -rf /tmp/OpenBLAS
